29b0001d320eec9bb6d559faa6847a7a21896108
edunietygic <eduniety.gic@gmail.com>
2017-04-18 21:26:17 +0900

    화이팅

---
+++ application/libraries/LoginClass.php
@@ -0,0 +1,81 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class LoginClass
+{
+    public function __construct()
+    {
+        $this->timestamp = date('YmdHis');     
+    }
+    
+    public  function portal($id, $pwd)
+    {
+        // live code
+        //$id  = $this->input->post('id');
+        //$pwd = $this->input->post('pwd');
+
+        if(! $this->_chkParam($id, $pwd))
+        {
+            echo "die"; 
+            die; 
+        }
+        if(! $sEnPwd = $this->_mkEncPWD($id, $pwd) ) 
+        {
+            echo "die"; 
+            die; 
+
+        }
+        $sFingerPrint = $this->_mkFingerPrint();
+        $sTimeStamp   = $this->timestamp; 
+
+        $this->_sendIB($id, $sFingerPrint, $sEnPwd, $sTimeStamp); 
+    }
+    private function _sendIB($id, $sFingerPrint, $sEnPwd, $sTimeStamp)
+    {
+ 
+        echo "send<br>";
+        echo "id : ".$id. "<br>";
+        echo "fingerpring : ".$sFingerPrint. "<br>";
+        echo "sEnPwd : ".$sEnPwd. "<br>";
+        echo "timestamp : ".$sTimeStamp. "<br>" ;
+        die;
+
+    }
+    private function _chkParam($id, $pwd)
+    {
+        if(!$id || !$pwd) return false;
+
+        return true;
+    }
+    private function _mkEncPWD($id, $pwd)
+    {
+        if(!$id || !$pwd) return false;
+
+        return "fjwiojfoewjfowiejfoijweiofjwojf23fj";
+    //$iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC);
+    //$enc_pwd = mcrypt_encrypt();
+
+    //return $enc_pwd;
+    }
+
+
+    private function _mkFingerPrint()
+    {
+        return md5($this->_getKey1().$this->timestamp.$this->_getKey2()); 
+    }
+
+    private function _getKey1()
+    {
+        return "eduniety";
+    }
+    private function _getKey2()
+    {
+        return "ib";
+    } 
+    private function _getKeyPwd()
+    {
+        $key = 'password-enc-key';
+        return $key;
+    } 
+
+
+}
