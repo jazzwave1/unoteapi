94457c7901f06450dc0ecef4fb657ae52729387b
edunietygic <eduniety.gic@gmail.com>
2017-04-26 11:51:12 +0900

    db info delete

--- application/config/database.php
+++ application/config/database.php
@@ -96,25 +96,4 @@ $db['default'] = array(
 );
 
 
-$db['dev_aws'] = array(
-	'dsn'	=> '',
-	'hostname' => '52.78.213.213',
-	'username' => 'eduniety',
-	'password' => '!@edunietygic@!',
-	'database' => 'eduniety',
-	'dbdriver' => 'mysqli',
-	'dbprefix' => '',
-	'pconnect' => FALSE,
-	'db_debug' => (ENVIRONMENT !== 'production'),
-	'cache_on' => FALSE,
-	'cachedir' => '',
-	'char_set' => 'utf8',
-	'dbcollat' => 'utf8_general_ci',
-	'swap_pre' => '',
-	'encrypt' => FALSE,
-	'compress' => FALSE,
-	'stricton' => FALSE,
-	'failover' => array(),
-	'save_queries' => TRUE
-);
 
