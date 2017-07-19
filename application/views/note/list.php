e238def8096d9038d9fb699b8d7023db57b107a1
jhseo <sseog818@gmail.com>
2017-07-19 16:53:59 +0900

    no message

---
+++ application/views/note/list.php
@@ -0,0 +1,31 @@
+<?php
+	$this->load->view('common/sub_timeline');
+
+	$this->load->view('common/textviewer');
+?>
+<script type="text/javascript">
+	$(function(){
+
+		$('.sublist-li').on('click', function(event){
+			$('.sublist-li').addClass('on');
+			var n_idx = $(this).data( "n_idx" );
+			$.post(
+			  "<?=HOSTURL?>/Note/rpcGetNoteInfo"
+			  ,{
+			       "n_idx" : n_idx 
+			   }
+			  ,function(data, status) {
+			    if (status == "success" && data.code == 1)
+			    {
+			        $('.p-date').text(data.aNoteDetail['regdate']);
+			        $('.p-tit').text(data.aNoteDetail['title']);
+			        $('.p-inner').html(data.aNoteDetail['text']);
+
+			        // console.log(data.aNoteDetail); 
+			    }
+			  }
+			);   
+		});
+
+	});
+</script>
