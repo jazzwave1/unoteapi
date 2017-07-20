b11608cab900625abb974bbce94219886b46cc6f
jhseo <sseog818@gmail.com>
2017-07-20 17:58:29 +0900

    no message

--- static/js/bucketEvent.js
+++ static/js/bucketEvent.js
@@ -32,3 +32,58 @@ function notiDelete(){
 $(".newWindowBtn").prop("href","http://www.daum.net");
 
 
+$(".noteDelBtn").on("click", noteDelete);
+
+function noteDelete()
+{
+    var n_idx = $('.p-info').data('n_idx');
+
+    if(confirm('삭제하시겠습니까? 삭제된 노트는 복구가 불가능합니다.'))
+    {
+        $.post(
+          "<?=HOSTURL?>/Note/deleteNote"
+          ,{
+               "n_idx" : n_idx 
+           }
+          ,function(data, status) {
+            if (status == "success" && data.code == 1)
+            {
+                alert(data.msg);
+                window.location.reload();
+                // console.log(data.aNoteDetail); 
+            }
+            // 삭제 실패
+            else if (status == "")
+            {
+                alert(data.msg);
+            }
+          }
+        );
+    }
+}
+
+$(function(){
+    $('.sublist-li').on('click', function(event){
+        $(this).siblings('li').removeClass('on');
+        $(this).addClass('on');
+        
+        var n_idx = $(this).data( "n_idx" );
+        $.post(
+          "<?=HOSTURL?>/Note/rpcGetNoteInfo"
+          ,{
+               "n_idx" : n_idx 
+           }
+          ,function(data, status) {
+            if (status == "success" && data.code == 1)
+            {
+                $('.p-info').data('n_idx', n_idx);
+                $('.p-date').text(data.aNoteDetail['regdate']);
+                $('.p-tit').text(data.aNoteDetail['title']);
+                $('.p-inner').html(data.aNoteDetail['text']);
+
+                // console.log(data.aNoteDetail); 
+            }
+          }
+        );   
+    });
+});
