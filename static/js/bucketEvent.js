678316136b6d7ddb9cfe2c5ba7831726b9c24fff
jhseo <sseog818@gmail.com>
2017-07-21 14:10:40 +0900

    jquery 수정

--- static/js/bucketEvent.js
+++ static/js/bucketEvent.js
@@ -62,28 +62,27 @@ function noteDelete()
     }
 }
 
-$(function(){
-    $('.sublist-li').on('click', function(event){
-        $(this).siblings('li').removeClass('on');
-        $(this).addClass('on');
-        
-        var n_idx = $(this).data( "n_idx" );
-        $.post(
-          "<?=HOSTURL?>/Note/rpcGetNoteInfo"
-          ,{
-               "n_idx" : n_idx 
-           }
-          ,function(data, status) {
-            if (status == "success" && data.code == 1)
-            {
-                $('.p-info').data('n_idx', n_idx);
-                $('.p-date').text(data.aNoteDetail['regdate']);
-                $('.p-tit').text(data.aNoteDetail['title']);
-                $('.p-inner').html(data.aNoteDetail['text']);
+$('.sublist-li').on('click', function(event){
+    $(this).siblings('li').removeClass('on');
+    $(this).addClass('on');
+    
+    var n_idx = $(this).data( "n_idx" );
 
-                // console.log(data.aNoteDetail); 
-            }
-          }
-        );   
-    });
+    $.post(
+      "rpcGetNoteInfo"
+      ,{
+           "n_idx" : n_idx 
+       }
+      ,function(data, status) {
+        if (status == "success" && data.code == 1)
+        {
+            $('.p-info').data('n_idx', n_idx);
+            $('.p-date').text(data.aNoteDetail['regdate']);
+            $('.p-tit').text(data.aNoteDetail['title']);
+            $('.p-inner').html(data.aNoteDetail['text']);
+
+            // console.log(data.aNoteDetail); 
+        }
+      }
+    );
 });
