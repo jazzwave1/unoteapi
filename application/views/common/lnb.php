f77c6169775420a303af53ee25c346354bc86726
edunietygic <eduniety.gic@gmail.com>
2017-07-22 14:16:17 +0900

    FB 크롤링 프론트 작업 완료

--- application/views/common/lnb.php
+++ application/views/common/lnb.php
@@ -181,7 +181,7 @@
                                     <fieldset>
                                         <input id="facebookToken" type="text" name="id" placeholder="토큰">
                                         <p class="log-formBtn">
-                                            <a href="javascript:callCrawl();">로그인</a>
+                                            <a href="javascript:FBCrawl();">로그인</a>
                                         </p>
                                         <p class="log-notice">
                                             * 아이디를 입력하세요 / * 비밀번호를 입력하세요 / * 입력정보를 확인해주세요
@@ -202,6 +202,13 @@
 function setSite(site)
 {
     $('#site').val(site);
+}
+function FBCrawl()
+{
+    var s_id  = "AT"; 
+    var s_pwd = $('#facebookToken').val(); 
+                
+    checkLoginState(); 
 }
 function callCrawl()
 {
@@ -248,3 +255,96 @@ function callCrawl()
 }      
 </script>
 
+<!--facebook login script-->
+<script>
+    // This is called with the results from from FB.getLoginStatus().
+    function statusChangeCallback(response) {
+        console.log('statusChangeCallback');
+        console.log(response);
+        console.log("UserId : "+response.authResponse.userID);
+        console.log("token : "+response.authResponse.accessToken);
+        // The response object is returned with a status field that lets the
+        // app know the current login status of the person.
+        // Full docs on the response object can be found in the documentation
+        // for FB.getLoginStatus().
+        if (response.status === 'connected') {
+            // Logged into your app and Facebook.
+            //testAPI();
+            document.getElementById('facebookToken').value = response.authResponse.accessToken;
+       
+            console.log('GOGO API~'); 
+            //document.fo.submit();
+            
+            callCrawl(); 
+        } else {
+            console.log('이건 뭔가요??'); 
+           // The person is not logged into your app or we are unable to tell.
+           //document.getElementById('status').innerHTML = 'Please log ' +
+           //  'into this app.';
+        }
+    }
+   
+    // This function is called when someone finishes with the Login
+    // Button.  See the onlogin handler attached to it in the sample
+    // code below.
+    function checkLoginState() {
+        FB.getLoginStatus(function(response) {
+            statusChangeCallback(response);
+        });
+    }
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '1853257284924341',
+            cookie     : true,  // enable cookies to allow the server to access 
+                                // the session
+            xfbml      : true,  // parse social plugins on this page
+            version    : 'v2.8' // use graph api version 2.8
+        });
+    // Now that we've initialized the JavaScript SDK, we call 
+    // FB.getLoginStatus().  This function gets the state of the
+    // person visiting this page and can return one of three states to
+    // the callback you provide.  They can be:
+    //
+    // 1. Logged into your app ('connected')
+    // 2. Logged into Facebook, but not your app ('not_authorized')
+    // 3. Not logged into Facebook and can't tell if they are logged into
+    //    your app or not.
+    //
+    // These three cases are handled in the callback function.
+    
+    // button acction 으로 처리 함 페이지 열리지 마자 하지 말자
+    //FB.getLoginStatus(function(response) {
+    //  statusChangeCallback(response);
+    //});
+    };
+
+    //FB.logout(function(response){
+    //
+    //});
+    
+    // Load the SDK asynchronously
+    (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/ko_KR/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk')); 
+
+    // Here we run a very simple test of the Graph API after login is
+    // successful.  See statusChangeCallback() for when this call is made.
+    //
+    function testAPI() {
+        console.log('Welcome!  Fetching your information.... ');
+        FB.api('/me', function(response) {
+        console.log('Successful login for: ' + response.name);
+        //document.getElementById('status').innerHTML =
+        'Thanks for logging in , ' + response.name + '!' ;
+        });
+
+    }
+</script> 
+    
+    
+    
+    
