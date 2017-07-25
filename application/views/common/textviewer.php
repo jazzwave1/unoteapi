6e2c6e60eea8013f91a6bb559a326e53bc6bdaae
ziyunn <jykim306@gmail.com>
2017-07-25 18:29:03 +0900

    no message

--- application/views/common/textviewer.php
+++ application/views/common/textviewer.php
@@ -24,12 +24,12 @@ $aCategory = $aMenuList['Category']['sub'];
                                         </div>
                                         <div class="p-btn">
                                             <ul class="clearfix">
-                                                <li class="<?=($controller == 'note') ? 'hide' : 'bookMark'?>"  title="북마크">
-                                                    <a class="bookMarkBtn" href="javascript:;"><i class="fa fa-bookmark fa-1g" aria-hidden="true"></i></a>
+                                                <li class="<?=($controller == 'note') ? 'hide' : 'bookMark'?>">
+                                                    <a class="bookMarkBtn" href="javascript:;" title="북마크"><i class="fa fa-bookmark fa-1g" aria-hidden="true"></i></a>
                                                     <!--<div>북마크</div>-->
                                                 </li>
-                                                <li class="<?=($controller == 'note') ? 'hide' : 'moveCateg'?>"  title="카테고리 이동">
-                                                    <a class="moveCategBtn" href="javascript:;"><i class="fa fa-clipboard fa-1g" aria-hidden="true"></i></a>
+                                                <li class="<?=($controller == 'note') ? 'hide' : 'moveCateg'?>">
+                                                    <a class="moveCategBtn" href="javascript:;" title="카테고리 이동"><i class="fa fa-clipboard fa-1g" aria-hidden="true"></i></a>
                                                     <!--<div>카테고리 이동</div>-->
                                                     <!--카테고리 이동 안내 창-->
                                                     <div class="selCateg">
@@ -50,18 +50,18 @@ $aCategory = $aMenuList['Category']['sub'];
                                                         </div>
                                                     </div><!--//카테고리 이동 안내 창-->
                                                 </li>
-                                                <li class="newWindow"  title="새창 열기">
-                                                    <a class="newWindowBtn" target="_blank" href="" onClick="window.open(this.href,'window','width=750,height=750,left=0,top=0')">
+                                                <li class="newWindow" >
+                                                    <a class="newWindowBtn" title="새창 열기" target="_blank" href="" onClick="window.open(this.href,'window','width=750,height=750,left=0,top=0')">
                                                         <i class="fa fa-external-link fa-1g " aria-hidden="true"></i>
                                                     </a>
                                                     <!--<div>새창</div>-->
                                                 </li>
-                                                <li class="copyLink"  title="링크 복사">
-                                                    <a class="copyLinkBtn" href="javascript:;"><i class="fa fa-link" aria-hidden="true"></i></a>
+                                                <li class="copyLink" >
+                                                    <a class="copyLinkBtn" title="링크 복사" href="javascript:;"><i class="fa fa-link" aria-hidden="true"></i></a>
                                                     <!--<div>링크복사</div>-->
                                                 </li>
-                                                <li class="<?=($controller == 'note') ? 'noteDelBtn' : 'articleDelBtn'?>"  title="휴지통">
-                                                    <a href="javascript:;"><i class="fa fa-trash-o fa-1g" aria-hidden="true"></i></a>
+                                                <li class="<?=($controller == 'note') ? 'noteDelBtn' : 'articleDelBtn'?>" >
+                                                    <a href="javascript:;" title="휴지통"><i class="fa fa-trash-o fa-1g" aria-hidden="true"></i></a>
                                                     <!--<div>휴지통</div>-->
                                                 </li>
                                             </ul>
