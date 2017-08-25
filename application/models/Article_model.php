061ab0fd6241665efe0d3a718adaec636c4c77fc
jhseo <sseog818@gmail.com>
2017-08-25 12:17:38 +0900

    참고글감 글자수 1000자 이상을 999+로 표시

--- application/models/Article_model.php
+++ application/models/Article_model.php
@@ -46,7 +46,8 @@ class  Article_model extends CI_model{
                     if(isset($oData->craw_data->contents))
                     {
                         $aArticleInfo[$key]->craw_data->title = mb_substr($oData->craw_data->contents, 0, 50, 'utf-8');;
-                        $aArticleInfo[$key]->craw_data->cnt = mb_strlen($oData->craw_data->contents);
+                        $cnt = mb_strlen($oData->craw_data->contents);
+                        $aArticleInfo[$key]->craw_data->cnt = ($cnt<1000) ? $cnt : '999+';
                     }
                     else
                     {
