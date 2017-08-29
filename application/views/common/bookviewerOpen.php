443163b809e150f6afea6d1b98eb8fe20fcee8c9
ziyunn <jykim306@gmail.com>
2017-08-29 13:31:58 +0900

    재단선 포함 미리보기용 페이지 작업

--- application/views/common/bookviewerOpen.php
+++ application/views/common/bookviewerOpen.php
@@ -1,1 +1,124 @@
-단행본 html
+<style>
+
+    /* Reset */
+    html,body{width:100%;height:100%; overflow:hidden}
+    html{overflow-y:auto;font-size:10px}
+    body,div,dl,dt,dd,ul,ol,li,h1,h2,h3,h4,h5,form,fieldset,p,button{margin:0;padding:0}
+    body,h1,h2,h3,h4,input,button{font-family:Malgun Gothic,'맑은고딕',Helvetica,AppleSDGothicNeo,sans-serif;font-size:14px;color:#555}
+    body{background-color:#fff;text-align:center;*word-break:break-all;-ms-word-break:break-all}
+    img,fieldset,iframe{border:0 none}
+    li{list-style:none}
+    input,select,button{vertical-align:middle}
+    img{vertical-align:top}
+    i,em,address{font-style:normal}
+    label,button{cursor:pointer}
+    button{margin:0;padding:0}
+    a{color:#555;text-decoration:none}
+    a:hover{text-decoration:none}
+    button *{position:relative}
+    button img{left:-3px;*left:auto}
+    html:first-child select{height:20px;padding-right:6px}
+    option{padding-right:6px}
+    hr{display:none}
+    legend{*width:0;display: none;}
+    table{border-collapse:collapse;border-spacing:0}
+    input::-ms-clear{display:none}
+    input {outline: none;border: 0px;}
+    *:focus {outline: none;}
+
+
+    .bookWrap{
+        width: 100%;
+        height:100%;
+        margin: 0 auto;
+        text-align: center;
+        background: #eee;
+    }
+    .bookBg{
+        position: relative;
+        width: 720px;
+        height:100%;
+        /*background: url('../../static/images/bg_book.png') no-repeat 50% 50%;
+        background-size: contain;*/
+        margin: 0 auto;
+    }
+    .bookBg img.bg{
+        width: 100%;
+        height: 100%;
+    }
+    .bookBg .bookTxt{
+        position: absolute;
+        top:0;
+        left: 0;
+        width: 720px;
+        height: 100%;
+    }
+    .bookBg .bookTxt .book-inner{
+       /* background: #ccc;*/
+        height: 100%;
+    }
+    .book-padding{
+        padding: 150px;
+        height: 650px;
+
+    }
+    .bookTop{
+        text-align: left;
+    }
+    .bookTop .tit{
+        height: 70px;
+        font-size: 18px;
+        font-weight: bold;
+    }
+    .bookBottom .txt{
+        height: 580px;
+        overflow: hidden;
+        text-align: justify;
+        line-height: 1.8;
+    }
+    .bookBottom .txt img{
+        width: 100%;
+        padding-bottom: 20px;
+    }
+</style>
+<div class="bookWrap">
+    <div class="bookBg">
+        <img class="bg" src="../../static/images/bg_book.png">
+        <div class="bookTxt">
+            <div class="book-inner">
+                <div class="book-padding">
+                    <div class="bookTop">
+                        <p class="tit">제목없는문서</p>
+                    </div>
+                    <div class="bookBottom">
+                        <div class="txt">
+                            <!--이미가 있을 경우-->
+                            <!--<img src="../../static/images/img_thum.jpg">-->
+                            LG디스플레이가 내년부터 벤츠, 아우디 등 독일 완성차 업체에 자동차용 유기발광다이오드(OLED) 조명 제품을 공급한다.
+                            오스람이 2015년 BMW에 후미등 용도로 OLED 조명 제품을 700대 한정 공급한 적은 있지만, 대량으로 OLED 조명이 자동차 제조사에 공급되는 것은 이번이 처음이다.
+                            LG디스플레이는 자동차 계기반, 중앙화면표시장치(CID) 등을 위한 OLED 디스플레이 패널을 공급하는 것은 물론 자동차 실내외 조명용으로 OLED를 공급할 예정이다.
+                            디스플레이 시장에서 OLED 확산에 나서고 있는 LG디스플레이는 급성장하는 자동차용 전장 부품 시장에 OLED 조명 제품을 공급함으로써 새로운 캐시카우를 만들 수 있을 것으로 기대했다.
+                            28일 업계에 따르면 LG디스플레이는 벤츠, 아우디, 폭스바겐 등 독일 완성차 업체들과 자동차용 실내외 인테리어와 조명용으로 OLED를 활용하는 기술을 개발하고 있다.
+                            LG디스플레이는 내년부터 이들 자동차 제조사에 차량용 OLED 조명을 본격 공급할 것으로 전해졌다.
+                            앞서 LG디스플레이는 최근 독일에서 플렉서블 OLED 조명 패널을 적용한 자동차 후미등 시제품을 공개하기도 했다.
+                            LG디스플레이는 내년부터 이들 자동차 제조사에 차량용 OLED 조명을 본격 공급할 것으로 전해졌다.
+                            앞서 LG디스플레이는 최근 독일에서 플렉서블 OLED 조명 패널을 적용한 자동차 후미등 시제품을 공개하기도 했다.
+                            LG디스플레이는 내년부터 이들 자동차 제조사에 차량용 OLED 조명을 본격 공급할 것으로 전해졌다.
+                            G디스플레이는 내년부터 이들 자동차 제조사에 차량용 OLED 조명을 본격 공급할 것으로 전해졌다.
+                            앞서 LG디스플레이는 최근 독일에서 플렉서블 OLED 조명 패널을 적용한 자동차 후미등 시제품을 공개하기도 했다.
+                            LG디스플레이는 내년부터 이들 자동차 제조사에 차량용 OLED 조명을 본격 공급할 것으로 전해졌다.
+                            G디스플레이는 내년부터 이들 자동차 제조사에 차량용 OLED 조명을 본격 공급할 것으로 전해졌다.
+                            앞서 LG디스플레이는 최근 독일에서 플렉서블 OLED 조명 패널을 적용한 자동차 후미등 시제품을 공개하기도 했다.
+                            LG디스플레이는 내년부터 이들 자동차 제조사에 차량용 OLED 조명을 본격 공급할 것으로 전해졌다.
+                            G디스플레이는 내년부터 이들 자동차 제조사에 차량용 OLED 조명을 본격 공급할 것으로 전해졌다.
+                            앞서 LG디스플레이는 최근 독일에서 플렉서블 OLED 조명 패널을 적용한 자동차 후미등 시제품을 공개하기도 했다.
+                            LG디스플레이는 내년부터 이들 자동차 제조사에 차량용 OLED 조명을 본격 공급할 것으로 전해졌다.G디스플레이는 내년부터 이들 자동차 제조사에 차량용 OLED 조명을 본격 공급할 것으로 전해졌다.
+                            앞서 LG디스플레이는 최근 독일에서 플렉서블 OLED 조명 패널을 적용한 자동차 후미등 시제품을 공개하기도 했다.
+                            LG디스플레이는 내년부터 이들 자동차 제조사에 차량용 OLED 조명을 본격 공급할 것으로 전해졌다.
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
