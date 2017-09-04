9ddfcdbfd7d1b8947f8be41cf28a9f052e6d9503
ziyunn <jykim306@gmail.com>
2017-09-04 11:44:04 +0900

    모바일 미리보기 전반적인 디자인 수정

--- application/views/common/textviewerOpen.php
+++ application/views/common/textviewerOpen.php
@@ -7,17 +7,27 @@
     .openWrap{
         position: relative;
         width: 100%;
-        background: #f6f6f6;
+        height: 100%;
         padding-bottom: 30px;
     }
     .bgWrap{
-        padding-top: 30px;
+        /*padding-top: 30px;*/
     }
-    .openView{
+    .openHeader{
+        position: relative;
+        overflow: hidden;
+        height: 500px;
+        background: #1a2226;
+    }
+    .openWrap .openView{
+        position: absolute;
+        top:100px;
+        left:50%;
         width: 800px;
         /*background: url("../../static/images/bg_notering.png") repeat-y 20px 0;*/
         z-index: 1000;
-        margin: 0 auto;
+        margin-left: -400px;
+        padding-bottom: 30px;
 
     }
 
@@ -32,7 +42,7 @@
         top:-26px;
         width: 700px;
         height:40px;
-        background: url("<?=SURL?>/images/bg_punch_top.png") repeat-x 8px 0;
+        /*background: url("<?=SURL?>/images/bg_punch_top.png") repeat-x 8px 0;*/
     }
     .openpage{
         height: 100%;
@@ -40,31 +50,77 @@
         box-shadow: 1px 4px 8px  #888888;
     }
     .open-wrap{
-        padding: 30px 30px 0px;
+        padding: 30px;
         text-align: justify;
     }
     .openpage .open-date{
         font-weight: bold;
-        padding: 10px 0;
+        padding: 10px 0 5px;
 
+    }
+    .openpage .writer{
+        padding-bottom: 50px;
+        border-bottom: 1px solid #ccc;
+        margin-bottom: 20px;
+    }
+    .openpage .writer .pic{
+        display: inline-block;
+        width: 40px;
+        height: 40px;
+
+    }
+    .openpage .writer .name{
+        display: inline-block;
+        padding-left: 10px;
+        font-weight: bold;
+        font-size: 12px;
+    }
+
+    .openpage .writer .pic img{
+        width: 100%;
+        border-radius: 50%;
+        vertical-align: middle;
     }
     .openpage .open-tit{
         font-weight: bold;
-        font-size: 20px;
+        font-size: 32px;
         padding-bottom: 20px;
     }
     .openpage .open-txt{
         line-height: 1.5;
     }
 
+    @media screen and (max-width: 800px){
+        .openWrap .openView{
+            position: absolute;
+            top:100px;
+            left: 5%;
+            width: 90%;
+            /*background: url("../../static/images/bg_notering.png") repeat-y 20px 0;*/
+            z-index: 1000;
+            margin-left: 0;
+            padding-bottom: 30px;
+
+        }
+        .openinner{
+            width: 100%;
+        }
+        .openpage{
+            box-shadow: 0px 0px 0px;
+        }
+
+    }
+
 </style>
 
 <div class="openWrap">
+    <div class="openHeader">
+    </div>
     <div class="bgWrap">
                 <!--detailView-->
                 <div id="detailView" class="openView">
                     <div class="openinner">
-                        <div class="page-bg"></div>
+                       <!-- <div class="page-bg"></div>-->
                         <div class="openpage">
                             <div class="open-wrap">
                                 <div class="open-top clearfix">
@@ -73,6 +129,9 @@
                                             <div class="open-info" data-n_idx="1" data-t_idx="1">
                                                 <p class="open-date"><?=$vdata['regdate']?></p>
                                                 <p class="open-tit"><?=$vdata['title']?></p>
+                                            </div>
+                                            <div class="writer">
+                                                <span class="pic"><img src="<?=SURL?>/images/junghun.jpg"></span><span class="name">정 훈</span>
                                             </div>
                                         </div>
                                     </div>
