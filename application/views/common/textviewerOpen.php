0ca2f99de6a428a2e29727add64793099b81312a
edunietygic <eduniety.gic@gmail.com>
2017-09-06 15:34:21 +0900

    공유 페이지 타이틀 적용

--- application/views/common/textviewerOpen.php
+++ application/views/common/textviewerOpen.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>공유</title>
+    <title><?=$vdata['title']?></title>
     <style>
         body,p{
             padding: 0px;
